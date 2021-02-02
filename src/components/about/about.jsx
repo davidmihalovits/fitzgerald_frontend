117fd7577e53b818a0274f2a8be99f457cd31763
Dávid Lajos Mihalovits <dmihalovits@gmail.com>
2021-02-02 10:03:10 +0100

    new

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,28 +3,24 @@ import ReactDOM from "react-dom";
 import "./about.sass";
 import Button from "@material-ui/core/button";
 import brian from "../../assets/brian.jpg";
-import reilly from "../../assets/reilly.jpg";
 import speares from "../../assets/speares.jpg";
 import kristen from "../../assets/kristen.jpg";
 import suz from "../../assets/suz.jpg";
 import Suz from "./modal/suz";
 import Kristen from "./modal/kristen";
 import Speares from "./modal/speares";
-import Reilly from "./modal/reilly";
 import Brian from "./modal/brian";
 
 const About = () => {
     const [showModalSuz, setShowModalSuz] = useState(false);
     const [showModalKristen, setShowModalKristen] = useState(false);
     const [showModalSpeares, setShowModalSpeares] = useState(false);
-    const [showModalReilly, setShowModalReilly] = useState(false);
     const [showModalBrian, setShowModalBrian] = useState(false);
 
     const closeModal = () => {
         setShowModalSuz(false);
         setShowModalKristen(false);
         setShowModalSpeares(false);
-        setShowModalReilly(false);
         setShowModalBrian(false);
     };
 
@@ -32,7 +28,6 @@ const About = () => {
         showModalSuz ||
         showModalKristen ||
         showModalSpeares ||
-        showModalReilly ||
         showModalBrian
     ) {
         document.getElementById("root").style.opacity = "0.2";
@@ -65,12 +60,6 @@ const About = () => {
                         setShowModalSpeares={setShowModalSpeares}
                     />
                 )}
-                {showModalReilly && (
-                    <Reilly
-                        closeModal={closeModal}
-                        setShowModalReilly={setShowModalReilly}
-                    />
-                )}
                 {showModalBrian && (
                     <Brian
                         closeModal={closeModal}
@@ -80,13 +69,7 @@ const About = () => {
             </React.StrictMode>,
             document.getElementById("modal")
         );
-    }, [
-        showModalBrian,
-        showModalReilly,
-        showModalSpeares,
-        showModalKristen,
-        showModalSuz,
-    ]);
+    }, [showModalBrian, showModalSpeares, showModalKristen, showModalSuz]);
 
     return (
         <div className="about-container">
@@ -102,98 +85,68 @@ const About = () => {
                     patients back to their active lifestyles.
                 </p>
                 <div className="members">
-                    <div className="brian-reilly-speares">
-                        <div className="member">
-                            <img
-                                className="member-image"
-                                src={brian}
-                                alt="brian"
-                            />
-                            <h2 className="member-name">Dr. Fitzgerald</h2>
-                            <p className="member-description">
-                                Sports Medicine, Shoulders, Neck, Back / Spinal
-                                Conditions, Knee, Ankle & Foot
-                            </p>
-                            <Button
-                                className="read-more-button"
-                                onClick={() => setShowModalBrian(true)}
-                            >
-                                Read more
-                            </Button>
-                        </div>
-                        <div className="member">
-                            <img
-                                className="member-image"
-                                src={reilly}
-                                alt="reilly"
-                            />
-                            <h2 className="member-name">Dr. Reilly</h2>
-                            <p className="member-description">
-                                Sports Medicine, Shoulders, Neck, Back / Spinal
-                                Conditions, Knee, Pediatrics and Pregnancy
-                            </p>
-                            <Button
-                                className="read-more-button"
-                                onClick={() => setShowModalReilly(true)}
-                            >
-                                Read more
-                            </Button>
-                        </div>
-                        <div className="member">
-                            <img
-                                className="member-image"
-                                src={speares}
-                                alt="speares"
-                            />
-                            <h2 className="member-name">Dr. Speares</h2>
-                            <p className="member-description">
-                                Sports Medicine, Shoulders, Neck, Back / Spinal
-                                Conditions, Dry Needling, Rehabilitation
-                            </p>
-                            <Button
-                                className="read-more-button"
-                                onClick={() => setShowModalSpeares(true)}
-                            >
-                                Read more
-                            </Button>
-                        </div>
+                    <div className="member">
+                        <img className="member-image" src={brian} alt="brian" />
+                        <h2 className="member-name">Dr. Fitzgerald</h2>
+                        <p className="member-description">
+                            Sports Medicine, Shoulders, Neck, Back / Spinal
+                            Conditions, Knee, Ankle & Foot
+                        </p>
+                        <Button
+                            className="read-more-button"
+                            onClick={() => setShowModalBrian(true)}
+                        >
+                            Read more
+                        </Button>
                     </div>
-                    <div className="kristen-suz">
-                        <div className="member">
-                            <img
-                                className="member-image"
-                                src={kristen}
-                                alt="kristen"
-                            />
-                            <h2 className="member-name">
-                                Kristen Magee PT, MSPT
-                            </h2>
-                            <p className="member-description">
-                                Rehabilitation and Therapeutic Exercise, Active
-                                Release Technique, Yoga and Dance Injuries
-                            </p>
-                            <Button
-                                className="read-more-button"
-                                onClick={() => setShowModalKristen(true)}
-                            >
-                                Read more
-                            </Button>
-                        </div>
-                        <div className="member">
-                            <img className="member-image" src={suz} alt="suz" />
-                            <h2 className="member-name">
-                                Suzanne - Office Manager
-                            </h2>
-                            <div
-                                style={{ height: "67.2px", marginTop: "15px" }}
-                            />
-                            <Button
-                                className="read-more-button"
-                                onClick={() => setShowModalSuz(true)}
-                            >
-                                Read more
-                            </Button>
-                        </div>
+                    <div className="member">
+                        <img
+                            className="member-image"
+                            src={speares}
+                            alt="speares"
+                        />
+                        <h2 className="member-name">Dr. Speares</h2>
+                        <p className="member-description">
+                            Sports Medicine, Shoulders, Neck, Back / Spinal
+                            Conditions, Dry Needling, Rehabilitation
+                        </p>
+                        <Button
+                            className="read-more-button"
+                            onClick={() => setShowModalSpeares(true)}
+                        >
+                            Read more
+                        </Button>
+                    </div>
+                    <div className="member">
+                        <img
+                            className="member-image"
+                            src={kristen}
+                            alt="kristen"
+                        />
+                        <h2 className="member-name">Kristen Magee PT, MSPT</h2>
+                        <p className="member-description">
+                            Rehabilitation and Therapeutic Exercise, Active
+                            Release Technique, Yoga and Dance Injuries
+                        </p>
+                        <Button
+                            className="read-more-button"
+                            onClick={() => setShowModalKristen(true)}
+                        >
+                            Read more
+                        </Button>
+                    </div>
+                    <div className="member">
+                        <img className="member-image" src={suz} alt="suz" />
+                        <h2 className="member-name">
+                            Suzanne - Office Manager
+                        </h2>
+                        <div style={{ height: "67.2px", marginTop: "15px" }} />
+                        <Button
+                            className="read-more-button"
+                            onClick={() => setShowModalSuz(true)}
+                        >
+                            Read more
+                        </Button>
                     </div>
                 </div>
             </div>
